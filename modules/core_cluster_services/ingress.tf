dbdf96bcccbfcd50120f03ac7ec700442dc64a23
Jason Field <jason@avon-lea.co.uk>
2020-10-04 10:05:19 +0100

    Use nginx

--- modules/core_cluster_services/ingress.tf
+++ modules/core_cluster_services/ingress.tf
@@ -7,38 +7,66 @@ resource "kubernetes_namespace" "ingress" {
   }
 }
 
-resource "helm_release" "traefik" {
-  name            = "traefik"
-  repository      = "https://helm.traefik.io/traefik"
-  chart           = "traefik"
+resource "helm_release" "nginx-ingress" {
+  name            = "nginx-ingress"
+  repository      = "https://helm.nginx.com/stable"
+  chart           = "nginx-ingress"
   namespace       = var.namespace_ingress
   wait            = true
   cleanup_on_fail = true
-
   set {
-    name  = "logs.level"
-    value = "INFO"
-  }
-
-  set {
-    name  = "logs.access.enabled"
+    name  = "rbac.create"
     value = true
   }
-
   set {
-    name = "dashboard.domain"
-    value = "traefik.174.138.103.44.nip.io"
+    name  = "rbac.createRole"
+    value = true
   }
+  set {
+    name  = "rbac.createClusterRole"
+    value = true
+  }
+  set {
+    name  = "controller.metrics.enabled"
+    value = true
+  }
+  set {
+    name  = "config.http2"
+    value = true
+  }
+  set {
+    name  = "config.redirect-to-https"
+    value = false
+  }
+  set {
+    name  = "config.proxy-buffer-size"
+    value = "12k"
+  }
+  set {
+    name  = "config.ssl-protocols"
+    value = "TLSv1.2"
+  }
+  set {
+    name  = "config.ssl-ciphers"
+    value = join(":", var.ssl_ciphers)
 
+  }
+  set {
+    name  = "config.ssl-prefer-server-ciphers"
+    value = true
+  }
+  set {
+    name  = "config.server-tokens"
+    value = false
+  }
 }
 
-data "kubernetes_service" "traefik" {
+data "kubernetes_service" "nginx-ingress" {
   metadata {
-    name      = "traefik"
+    name      = "nginx-ingress-nginx-ingress"
     namespace = "ingress"
   }
-
   depends_on = [
-    helm_release.traefik,
+    helm_release.nginx-ingress,
   ]
 }
