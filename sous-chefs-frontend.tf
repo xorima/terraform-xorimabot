272ec14684a7f799378232a82b83f0ffce3804f7
Jason Field <jason@avon-lea.co.uk>
2020-10-04 13:32:52 +0100

    Set DNS record correctly

--- sous-chefs-frontend.tf
+++ sous-chefs-frontend.tf
@@ -114,9 +114,9 @@ resource "kubernetes_ingress" "labelvalidator-sous-chef-frontend" {
   }
 }
 
-resource "cloudflare_record" "charts" {
+resource "cloudflare_record" "labelvalidator-sous-chef-frontend" {
   zone_id = local.cloudflare_dns_zone_id
-  name    = "charts"
+  name    = var.host_labelvalidator
   value   = local.kubernetes_public_ip
   type    = "A"
   ttl     = 1
