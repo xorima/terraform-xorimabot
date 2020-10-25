24570dac0bedfcba9a81140a91cf3caca16ab8a9
Jason Field <4923914+Xorima@users.noreply.github.com>
2020-10-25 15:06:13 +0000

    Enable changelog reset for Xorima (#11)
    * Enable changelog reset for Xorima
    
    * Create hostname

--- xorima-frontend.tf
+++ xorima-frontend.tf
@@ -3,6 +3,7 @@ locals {
     label_validator     = "${var.app_hostname_prefix.label_validator}.xorima.${local.domain_config.base_domain}"
     json_version_bumper = "${var.app_hostname_prefix.json_version_bumper}.xorima.${local.domain_config.base_domain}"
     release_creator     = "${var.app_hostname_prefix.release_creator}.xorima.${local.domain_config.base_domain}"
+    changelog_reset     = "${var.app_hostname_prefix.changelog_reset}.xorima.${local.domain_config.base_domain}"
   }
 }
 
@@ -79,6 +80,23 @@ module "xorima-release-creator" {
 resource "cloudflare_record" "release-creator-xorima-frontend" {
   zone_id = local.cloudflare_dns_zone_id
   name    = local.xorima_hostnames.release_creator
+  value   = local.kubernetes_public_ip
+  type    = "A"
+  ttl     = 1
+}
+
+module "xorima-changelog-reset" {
+  source             = "./modules/changelog_reset"
+  kube_config        = local.kube_config
+  namespace          = kubernetes_namespace.xorima-frontend.metadata[0].name
+  app_version        = local.app_version.changelog_reset
+  github_secret_name = kubernetes_secret.webhook-github-xorima-frontend.metadata[0].name
+  hostname           = local.xorima_hostnames.changelog_reset
+}
+
+resource "cloudflare_record" "changelog-reset-xorima-frontend" {
+  zone_id = local.cloudflare_dns_zone_id
+  name    = local.xorima_hostnames.changelog_reset
   value   = local.kubernetes_public_ip
   type    = "A"
   ttl     = 1
